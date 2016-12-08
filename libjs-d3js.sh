2793679128111427311c717420c90d1b918deed0
vitex <vitex@kickass.vitexsoftware.cz>
2016-12-08 16:39:40 +0100

    Now we get libs by node install

--- libjs-d3js.sh
+++ libjs-d3js.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
+npm install d3
 
-if [ -d "d3" ]; then
-  cd d3
-  git pull
-else
-  git clone https://github.com/mbostock/d3.git
-  cd d3
-fi
-
-npm install
-npm run build
-npm run dist
-
-VERSION=`cat package.json | grep '"version":' | awk -F '"' '{ print $4 }'`
-#BUILD=`cat  ./build | perl -ne 'chomp; print join(".", splice(@{[split/\./,$_]}, 0, -1), map {++$_} pop @{[split/\./,$_]}), "\n";'`
-#echo $BUILD > build
+VERSION=`cat node_modules/d3/build/package.js | grep "export var version" | cut -d'"' -f 2`
+BUILD=`cat  ./build | perl -ne 'chomp; print join(".", splice(@{[split/\./,$_]}, 0, -1), map {++$_} pop @{[split/\./,$_]}), "\n";'`
+echo $BUILD > build
 BUILD=1
 
 #CHANGES=`cat CHANGELOG.md | awk -vRS="##" 'NR<=2' ORS="##" | grep -v "##"`
-tar -czf ../../libjs-d3js_$VERSION.orig.tar.gz .
-cd ..
+#tar -czf ../../libjs-d3js_$VERSION.orig.tar.gz .
+
 #dch -v $VERSION-1 --package libjs-twitter-bootstrap $CHANGES
 dch -v $VERSION-$BUILD --package libjs-d3js "Debianized $VERSION"
 
